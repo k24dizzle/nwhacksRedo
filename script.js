270b59435ec27b70db0ace4299cdde852ce67b5c
antonzheng <anton.zheng@gmail.com>
2018-01-14 00:47:04 -0800

    Almost working front-end

--- script.js
+++ script.js
@@ -3,12 +3,16 @@
 
     window.onload = function() {
         initEventListeners();
+        var canvas = $("canvas");
+        $("#live_container").append(canvas);
   	}
 
     function initEventListeners() {
         $("#landing_cta").click(function() {
             $("#landing_container").css("display", "none");
             $("#demo_container").css("display", "block");
+            $("canvas").css("display", "block");
+            $("#connect-leap").css("display", "block");
         });
     }
 
